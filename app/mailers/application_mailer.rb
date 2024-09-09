cdd2117c81bfd9e1393cbb2c722d4c2de4f53069
Andrew Yacoub <andrewyacoub95@gmail.com>
2024-09-09 16:38:45 +0300

    resolving spam issue

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "from@example.com", "Message-ID" => lambda {"<#{SecureRandom.uuid}@#{ActionMailer::Base.smtp_settings[:domain]}>"}
   layout "mailer"
 end
